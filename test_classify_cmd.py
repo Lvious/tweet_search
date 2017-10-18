918f7eda63a1427d722c3ab4ee7cca441c9eec31
phiedulxp <phiedulxp@gmail.com>
2017-10-18 17:30:07 +0800

    test_classify_cmd.py

--- test_classify_cmd.py
+++ test_classify_cmd.py
@@ -4,11 +4,7 @@ import fire
 
 def main(num=8):
 	for i in range(8):
-		p = subprocess.Popen(['python','test_classify.py'],shell=False, 
-						stdout=subprocess.PIPE,
-						stderr=subprocess.PIPE)
-		result,error = p.communicate()
-		print result,error
+		subprocess.Popen(['python','test_classify.py'])
 
 if __name__ == '__main__':
 	fire.Fire(main)

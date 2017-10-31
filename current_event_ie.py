3ea7c9e912d9a2ec370d778cbd98450035c97950
phiedulxp <phiedulxp@gmail.com>
2017-10-31 19:38:06 +0800

    get_best_ner_openie.ipynb

--- current_event_ie.py
+++ current_event_ie.py
@@ -2,7 +2,7 @@ from pycorenlp import StanfordCoreNLP
 nlp = StanfordCoreNLP('http://localhost:9000')
 
 from datetime import datetime,timedelta
-from collections import defaultdict
+from collections import defaultdict,Counter
 from pprint import pprint
 from tqdm import tqdm
 import re
@@ -38,53 +38,70 @@ def get_ner_dict(word,ner):
 			ner_dict[nt[0][1]].append(' '.join([i[0] for i in nt]))
 	return dict(ner_dict)
 	
+def get_most_common(items):
+	count = Counter(items)
+	return count.most_common()[0][0]
+	
+def best_openie(openies):
+	if len(openies) == 0:
+		return {}
+	openie_df = pd.DataFrame.from_dict(openies)
+	object_lsit = openie_df['object'].tolist()
+	object_,lenth  = object_lsit[0],len(object_lsit[0])
+	for i in object_lsit[1:]:
+		if len(i) > lenth:
+			object_,lenth = i,len(i)
+	return {'subject':get_most_common(openie_df.subject),
+			'relation':get_most_common(openie_df.relation),
+			'object':object_}
+	
 def ie_pipeline(text):
 	output = nlp.annotate(text, properties={
-						  'annotators': 'truecase,ner,sentiment,openie',
+						  'annotators': 'truecase,ner,openie',#sentiment
 						  'outputFormat': 'json',
 						  })
 	sentences = output['sentences']
 	if len(sentences) == 0:
 		return {
-		'word':{},
-		'truecaseText':{},
-		'lemma':{},
-		'pos':{},
-		'ner':{},
+		#'word':{},
+		#'truecaseText':{},
+		#'lemma':{},
+		#'pos':{},
+		#'ner':{},
 		'ner_list':{},
 		'openie':{},
-		'sentiment':{},
+		#'sentiment':{},
 				}
 	words = []
-	truecaseTexts = []
-	lemmas = []
-	poss = []
+	#truecaseTexts = []
+	#lemmas = []
+	#poss = []
 	ners = []
 	openies = []
-	sentiments = []
+	#sentiments = []
 	for sentence in sentences:
 		tokens_df = pd.DataFrame.from_dict(sentence['tokens'])
 		words.extend(tokens_df['word'].tolist())
-		truecaseTexts.extend(tokens_df['truecaseText'].tolist())
-		lemmas.extend(tokens_df['lemma'].tolist())
-		poss.extend(tokens_df['pos'].tolist())
+		#truecaseTexts.extend(tokens_df['truecaseText'].tolist())
+		#lemmas.extend(tokens_df['lemma'].tolist())
+		#poss.extend(tokens_df['pos'].tolist())
 		ners.extend(tokens_df['ner'].tolist())
 		for i in sentence['openie']:
 			temp = i.pop('objectSpan')
 			temp = i.pop('relationSpan')
 			temp = i.pop('subjectSpan')
 		openies.extend(sentence['openie'])
-		sentiments.append((sentence['sentiment'],sentence['sentimentValue']))
+		#sentiments.append((sentence['sentiment'],sentence['sentimentValue']))
 	ner_dict = get_ner_dict(words,ners)
 	return {
-		'word':words,
-		'truecaseText':truecaseTexts,
-		'lemma':lemmas,
-		'pos':poss,
-		'ner':ners,
+		#'word':words,
+		#'truecaseText':truecaseTexts,
+		#'lemma':lemmas,
+		#'pos':poss,
+		#'ner':ners,
 		'ner_dict':ner_dict,
-		'openie':openies,
-		'sentiment':sentiments,
+		'openie':best_openie(openies),
+		#'sentiment':sentiments,
 	}
 
 def batch_ie(texts):
@@ -93,13 +110,16 @@ def batch_ie(texts):
 		ies.append(ie_pipeline(re.sub('http.+','',text.encode('utf-8'))))
 	return ies
 	
+def clear_description(des):
+	return re.sub('\([\w ]+\)','',des).strip()
+	
 if __name__ == '__main__':
-	query = db.current_event.find({'ie':None},{'_id':1,'event.description':1})
+	query = db.current_event.find({'ie':None},{'_id':1,'event.title':1,'event.description':1})
 	ids = []
 	texts = []
 	for i in query:
 		ids.append(i['_id'])
-		texts.append(i['event']['description'])
+		texts.append(i['event']['description']+'. '+clear_description(i['event']['description']))
 	ies = batch_ie(texts)
 	requests = [UpdateOne({'_id': _id,'ie':None}, {'$set': {'ie':ies[index]}}) for index,_id in tqdm(enumerate(ids))]
 	try:

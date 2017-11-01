116f40830192e0379bf5b057c22f7e15af2ba2ad
phiedulxp <phiedulxp@gmail.com>
2017-11-01 15:39:23 +0800

    test

--- master_user.py
+++ master_user.py
@@ -14,7 +14,7 @@ r = redis.StrictRedis(host='52.91.102.254', port=6379, db=0)
 
 def get_task():
 	for type_id in tqdm(range(10)[1:]):
-		for user in tqdm(db.train_user.find({'_id':type_id})['list']):
+		for user in tqdm(db.train_user.find_one({'_id':type_id})['list']):
 			message = {'type_id':type_id,'q':'from:'+user,'f':'&f=tweets','num':-1}
 			r.rpush('task:user',json.dumps(message))
 

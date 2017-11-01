09c724957aa2fe75ea1514002ba33bfaba87bfe9
phiedulxp <phiedulxp@gmail.com>
2017-11-01 15:18:19 +0800

    user

---
+++ master_user.py
@@ -0,0 +1,22 @@
+import re
+import json
+import fire
+from tqdm import tqdm
+from datetime import datetime,timedelta
+
+import pymongo
+client = pymongo.MongoClient('101.132.187.45:27017')
+db = client.twitter
+
+import redis
+r = redis.StrictRedis(host='52.91.102.254', port=6379, db=0)
+
+
+def get_task():
+	for type_id in tqdm(range(10)[1:]):
+		for user in tqdm(db.train_user.find({'_id':type_id})['list']):
+			message = {'type_id':type_id,'q':'from:'+user,'f':'&f=tweets','num':-1}
+			r.rpush('task:user',json.dumps(message))
+
+if __name__ == '__main__':
+	get_task()
---
+++ worker_user.py
@@ -0,0 +1,58 @@
+import sys
+if sys.version_info[0] < 3:
+	import got
+else:
+	import got3 as got
+    
+import time
+import json
+    
+import multiprocessing
+from multiprocessing import Pool
+
+import pymongo
+client = pymongo.MongoClient('101.132.187.45:27017')
+db = client.twitter
+
+import redis
+r = redis.StrictRedis(host='52.91.102.254', port=6379, db=0)
+
+
+def advance_search_test(type_id,q,f,num):
+	client = pymongo.MongoClient('101.132.187.45:27017')
+	db = client.twitter
+	collection = [db.Entertainment,db.Religion,db.Sport,db.Military,db.Politics,db.Education,db.Technology,db.Economy,db.Agriculture][type_id-1]
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch(q).setTweetType(f).setMaxTweets(num)
+	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+	for tweet in tweets:
+		if collection.find_one({'_id':tweet.id}) == None:
+			collection.insert_one({'_id':tweet.id,'tweet':tweet.__dict__,'f':f,'q':q})
+
+def run_user_task(message_data):
+	try:
+		q = message_data['q']
+		num = message_data['num']
+		type_id  = message_data['type_id']
+		if type(message_data['f']) != list:
+			advance_search_test(type_id,q,message_data['f'],num)
+		else:
+			pool = Pool(processes=multiprocessing.cpu_count())
+			[pool.apply(advance_search_test,(q,f,num)) for f in message_data['f']]
+			pool.close()
+			pool.join()
+	except Exception,e:
+		raise Exception(e.message)
+  
+if __name__ == '__main__':
+	print 'craw_worker start!'
+	while True:
+		queue = r.lpop('task:user')
+		if queue:
+			print 'craw_worker process!'
+			try:
+				run_user_task(json.loads(queue))
+				db.user_log.insert_one({'message':json.loads(queue),'status':1})
+			except Exception,e:
+				db.user_log.insert_one({'message':json.loads(queue),'status':0,'error':e.message})
+		time.sleep(1)
+		print 'craw_worker wait!'

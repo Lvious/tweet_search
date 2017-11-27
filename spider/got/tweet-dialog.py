655c677754c65c1ba50fb9080ba3400566a7805e
phiedulxp <phiedulxp@gmail.com>
2017-11-27 15:27:08 +0800

    index

---
+++ spider/got/tweet-dialog.py
@@ -0,0 +1,23 @@
+<div> class="PermalinkOverlay-modal" id="permalink-overlay-dialog" role="dialog" aria-labelledby="permalink-overlay-header" </div>
+<div class="PermalinkOverlay-content" role="document">
+      <div class="PermalinkOverlay-body" data-background-path="/hoorfre">
+      <div role="main" class="permalink light-inline-actions
+  stream-uncapped
+  has-replies
+  original-permalink-page
+  
+  
+  ">
+  
+  <div class="permalink-inner permalink-tweet-container">  ##原始推文
+  <div class="replies-to  permalink-inner permalink-replies" data-component-context="replies"> ##所有回复
+  
+  <div class="tweets-wrapper">
+      <div id="descendants" class="ThreadedDescendants">
+          <div class="stream-container  " data-max-position="">
+    <div class="stream">
+        <ol class="stream-items js-navigable-stream" id="stream-items-id">  ## 回复开始
+          <li class="ThreadedConversation">
+              <ol class="stream-items">
+              <div>  ## 回复内容
+              <li>   ## 另外一条回复

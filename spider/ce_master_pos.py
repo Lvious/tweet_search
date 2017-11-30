adce98afc5fc81c3aa8a736b73ab6d55a19423d1
phiedulxp <phiedulxp@gmail.com>
2017-11-30 10:51:28 +0800

    update

--- spider/ce_master_pos.py
+++ spider/ce_master_pos.py
@@ -9,12 +9,12 @@ _,db,r = get_config(
 
 
 def get_task():
-    for item in tqdm(db.current_event.find({'type':{'$gte':1}},{'_id':1,'query_str':1})):
-        q = item['query_str']
-        message = {'q':q,'f':['&f=news','','&f=tweets'],'num':1000,'event_id':item['_id']}
-        print message
-        r.rpush('task:pos',json.dumps(message))
+	for item in tqdm(db.current_event.find({'type':{'$gte':1}},{'_id':1,'query_str':1})):
+		q = item['query_str']
+		message = {'q':q,'f':['&f=news','','&f=tweets'],'num':1000,'event_id':item['_id']}
+		print message
+		r.rpush('task:pos',json.dumps(message))
 
 if __name__ == '__main__':
-    get_task()
+	get_task()
 

6f32932ee8c2cd04411701c68836567e0bb204a7
phiedulxp <phiedulxp@gmail.com>
2017-11-30 16:28:18 +0800

    update

--- preprocessing/mongo_cli.py
+++ preprocessing/mongo_cli.py
@@ -1,2 +1,3 @@
 db.cloneCollection('ip:host','from coll','to coll')
 db.copyDatabase('from db','to db','ip:host')
+db.copyDatabase(<from_dbname>, <to_dbname>, <from_hostname>, <username>,<password>);

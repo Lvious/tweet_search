eae32f60e7e7d61ed33bdbbb28a5fe0094f9de3b
phiedulxp <phiedulxp@gmail.com>
2017-11-30 10:02:52 +0800

    ce_master_pos.py

---
+++ spider/ce_master_pos.py
@@ -0,0 +1,20 @@
+import re
+import json
+from datetime import datetime,timedelta
+
+from tqdm import tqdm
+
+from Config import get_config
+_,db,r = get_config(
+
+
+def get_task():
+    for item in tqdm(db.current_event.find({'type':{'$gte':1}},{'_id':1,'query_str':1})):
+        q = item['query_str']
+        message = {'q':q,'f':['&f=news','','&f=tweets'],'num':1000,'event_id':item['_id']}
+        print message
+        r.rpush('task:pos',json.dumps(message))
+
+if __name__ == '__main__':
+    get_task()
+

e28eb829a9fb09fed91ae8f997ee2f20aced0913
phiedulxp <phiedulxp@gmail.com>
2017-12-02 17:29:41 +0800

    update

--- spider/Config.py
+++ spider/Config.py
@@ -12,11 +12,11 @@ import os
 
 def get_config():
 	#mongo
-	client = pymongo.MongoClient(os.environ('MONGOHOST'),27017)
+	client = pymongo.MongoClient(os.environ['MONGOHOST'],27017)
 	db = client.tweet
 	db.authenticate(name='admin',password='lixiepeng')
 	
 	#redis
-	r = redis.StrictRedis(host=os.environ('REDISHOST'), port=6379, db=0, password='lixiepeng')
+	r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0, password='lixiepeng')
 	
 	return got,db,r

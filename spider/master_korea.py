53602ab864a8a7d82a47a30411586141cdafd069
lvious <lvxq777@gmail.com>
2018-01-04 18:41:27 +0800

    master word combine

--- spider/master_korea.py
+++ spider/master_korea.py
@@ -11,25 +11,25 @@ _,db,r = get_spider_config()
 
 
             
-def get_query_str(loc,trigger):
+def get_query_str(loc,triggers,target):
 	# start = (now - time_delta).strftime("%Y-%m-%d %H:%M:%S")
 	# now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-	return '('+' OR '.join(loc)+')' + ' '+'('+' OR '.join(trigger)+')'
-
+	return '('+loc+')' + + ' '+'('+' OR '.join(triggers)+')'+' '+'('+target+')'
 
 def get_task():
     locs=["North Korea"]
-    triggers=["test nuclear","launch","fire","messile","satellite","rocket"]
+    triggers=["test","launch","fire"]
+    target = ["messile","satellite","rocket","nuclear"]
     while True:
         now = datetime.now()
         for loc in locs:
-            for trigger in triggers:
-                q = get_query_str(locs,triggers)
+            for target in targets:
+                q = get_query_str(loc,triggers,target)
                 message = {'q':q,'f':['&f=news','','&f=tweets'],'num':1000,
-                "sinceTimeStamp":(now - timedelta(minutes=15)).strftime("%Y-%m-%d %H:%M:%S"),
+                "sinceTimeStamp":(now - timedelta(minutes=60)).strftime("%Y-%m-%d %H:%M:%S"),
                 "untilTimeStamp":now.strftime("%Y-%m-%d %H:%M:%S")
                 }
                 r.rpush("task:korea",json.dumps(message))
-        time.sleep(15*60)
+        time.sleep(60*60)
 if __name__ == '__main__':
 	get_task()

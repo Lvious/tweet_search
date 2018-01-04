980e98960d59b7211d81f0b13e992d74aed25157
lvious <lvxq777@gmail.com>
2018-01-04 20:05:18 +0800

    add frequsers

--- spider/master_korea.py
+++ spider/master_korea.py
@@ -9,12 +9,10 @@ from Config import get_spider_config
 _,db,r = get_spider_config()
 
 
-
+freq_users = set([i['tweet']['user']['screen_name'] for i in db.dataset_korea_m_1.find({},{"tweet.user.screen_name":1})])
             
-def get_query_str(loc,triggers,target):
-	# start = (now - time_delta).strftime("%Y-%m-%d %H:%M:%S")
-	# now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-	return '('+loc+')' + + ' '+'('+' OR '.join(triggers)+')'+' '+'('+target+')'
+def get_query_str(loc,triggers,target,user):
+	return '('+loc+')' + + ' '+'('+' OR '.join(triggers)+')'+' '+'('+target+')'+"from "+user
 
 def get_task():
     locs=["North Korea"]
@@ -22,14 +20,15 @@ def get_task():
     target = ["messile","satellite","rocket","nuclear"]
     while True:
         now = datetime.now()
-        for loc in locs:
-            for target in targets:
-                q = get_query_str(loc,triggers,target)
-                message = {'q':q,'f':['&f=news','','&f=tweets'],'num':1000,
-                "sinceTimeStamp":(now - timedelta(minutes=60)).strftime("%Y-%m-%d %H:%M:%S"),
-                "untilTimeStamp":now.strftime("%Y-%m-%d %H:%M:%S")
-                }
-                r.rpush("task:korea",json.dumps(message))
+        for user in freq_users:
+            for loc in locs:
+                for target in targets:
+                    q = get_query_str(loc,triggers,target,user)
+                    message = {'q':q,'f':['&f=news','','&f=tweets'],'num':1000,
+                    "sinceTimeStamp":(now - timedelta(minutes=60)).strftime("%Y-%m-%d %H:%M:%S"),
+                    "untilTimeStamp":now.strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    r.rpush("task:korea",json.dumps(message))
         time.sleep(60*60)
 if __name__ == '__main__':
 	get_task()

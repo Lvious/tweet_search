0d99222ad815c62ebfd389dc9dde8102d0cb82c7
Lvious <lvxq777@gmail.com>
2018-01-05 14:18:37 +0800

    Update master_korea.py

--- spider/master_korea.py
+++ spider/master_korea.py
@@ -4,12 +4,13 @@ import fire
 import time
 from tqdm import tqdm
 from datetime import datetime,timedelta
-
+from collections import Counter
 from Config import get_spider_config
 _,db,r = get_spider_config()
 
 
-freq_users = set([i['tweet']['user']['screen_name'] for i in db.dataset_korea_m_1.find({},{"tweet.user.screen_name":1})])
+users = [i['tweet']['user']['screen_name'] for i in db.dataset_korea_m_1.find({},{"tweet.user.screen_name":1})]
+freq_users = [i[0] for i in Counter(users).most_common() if i[1]>=5]
             
 def get_query_str(loc,triggers,target):
 	# start = (now - time_delta).strftime("%Y-%m-%d %H:%M:%S")

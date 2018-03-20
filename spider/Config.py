6a95a1c727cc16989049023b037244ef717447ea
lvious <lvxq777@gmail.com>
2018-03-20 11:18:41 +0800

    add

--- spider/Config.py
+++ spider/Config.py
@@ -15,13 +15,13 @@ def get_spider_config():
 		import got3 as got
 		
 	#mongo
-	client = pymongo.MongoClient(os.environ['MONGOHOST'],os.environ['MONGOPORT'])
+	client = pymongo.MongoClient(os.environ['MONGOHOST'],int(os.environ['MONGOPORT']))
 	db = client.tweet
 	#db.authenticate(name='admin',password='lixiepeng')
 	
 	#redis
 	#r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0, password='lixiepeng')
-	r = redis.StrictRedis(host=os.environ['REDISHOST'], port=os.environ['REDISPORT'], db=0)
+	r = redis.StrictRedis(host=os.environ['REDISHOST'], port=int(os.environ['MONGOPORT']), db=0)
 
 	return got,db,r
 
@@ -33,12 +33,13 @@ def get_fk_config():
 		import got3 as got
 		
 	#mongo
-	client = pymongo.MongoClient(os.environ['MONGOHOST'],os.environ['MONGOPORT'])
-	db = client.tweet
+	client = pymongo.MongoClient(os.environ['MONGOHOST'],int(os.environ['MONGOPORT']))
+	db = client.terror
 	#db.authenticate(name='admin',password='lixiepeng')
 	
 	#redis
 	#r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0, password='lixiepeng')
-	r = redis.StrictRedis(host=os.environ['REDISHOST'], port=os.environ['REDISPORT'], db=0)
+	r = redis.StrictRedis(host=os.environ['REDISHOST'], port=int(os.environ['MONGOPORT']), db=0)
 
 	return got,db,r
+
